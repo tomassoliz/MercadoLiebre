288395388c533fce18c86f86b22e2f467ce6c1d5
Tomas Soliz <tomassoliz07@gmail.com>
2023-06-24 13:24:54 -0400

    subiendo Mercado Liebre

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = 3030
+
+app.use(express.static('public'))
+
+app.get('/',(req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
+app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`))

67bfad32ae411d1981b1a665c9edb487a14c0c79
Tomas Soliz <tomassoliz07@gmail.com>
2023-07-21 02:53:55 -0300

    completando formulario y flex

--- app.js
+++ app.js
@@ -5,5 +5,7 @@ const PORT = 3030
 
 app.use(express.static('public'))
 
-app.get('/',(req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
-app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`))
+app.get('/',(req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/register', (req, res) => { res.sendFile(path.join(__dirname, "views", "register.html")) });
+app.get('/inicio', (req, res) => { res.sendFile(path.join(__dirname, "views", "inicio.html")) });
+app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`));

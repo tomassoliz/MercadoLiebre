25400b06b19e4243696d9185825a0a73fe5aaefb
Tomas Soliz <tomassoliz07@gmail.com>
2023-07-30 17:53:59 -0300

    agregando transiciones

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.use(express.static('public'))
 
 app.get('/',(req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
 app.get('/register', (req, res) => { res.sendFile(path.join(__dirname, "views", "register.html")) });
-app.get('/inicio', (req, res) => { res.sendFile(path.join(__dirname, "views", "inicio.html")) });
+app.get('/login', (req, res) => { res.sendFile(path.join(__dirname, "views", "login.html")) });
 app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`));
